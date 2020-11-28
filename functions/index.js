c3c286d37ab20b462d2a451e4f582b6f02336732
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-11-28 17:25:09 +0900

    create function to create and delete user in firestore

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// add admin role
 exports.addAdminRole = functions.https.onCall((data, context) => {
+	// can only be called from admin role
 	if (context.auth.token.admin !== true) {
 		return { error: 'Only admins can add other admins!' };
 	}
@@ -21,4 +23,17 @@ exports.addAdminRole = functions.https.onCall((data, context) => {
 		.catch(err => {
 			return err;
 		});
+});
+
+// auth trigger to create user in firestore on signup
+exports.newUserSignup = functions.auth.user().onCreate(user => {
+	return admin.firestore().collection('users').doc(user.uid).set({
+		likedPhotos: [],
+	});
+});
+
+// auth triffer to delete user in firestore
+exports.userDeleted = functions.auth.user().onDelete(user => {
+	const doc = admin.firestore().collection('users').doc(user.uid);
+	return doc.delete();
 });
